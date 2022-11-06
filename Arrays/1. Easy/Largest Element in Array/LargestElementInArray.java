dac32e88f466bdae8a3d55689317e86312f68fb5
Gokul R <gk.rsjg@gmail.com>
2022-11-06 17:34:01 +0530

    Largest Element in Array

---
+++ Arrays/1. Easy/Largest Element in Array/LargestElementInArray.java
@@ -0,0 +1,16 @@
+import org.omg.CORBA.FieldNameHelper;
+
+public class LargestElementInArray{
+    public static int FindLargest(int arr[]){
+        //Assuming the array contains only positive integer
+        int max = 0;
+        for(int curr : arr){
+            if(curr>max) max = curr;
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int array[] = {2,5,1,3,0};
+        System.out.println(FindLargest(array));
+    }
+}
