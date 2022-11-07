bf9cf43bb66f700252877ab863ca40437b2a31d5
Gokul R <gk.rsjg@gmail.com>
2022-11-07 17:35:21 +0530

    Second Largest Element in array

---
+++ Arrays/1. Easy/Second Largest Element in Array(no Sorting)/SecondLargestElement.java
@@ -0,0 +1,17 @@
+public class SecondLargestElement{
+    public static int FindSecondLargest(int arr[]){
+        int max = 0;
+        int SeconeLarge = 0;
+        for(int curr : arr){
+            if(max < curr) max = curr;
+        }
+        for(int curr : arr){
+            if(curr > SeconeLarge && curr< max) SeconeLarge = curr;
+        }
+        return SeconeLarge;
+    }
+    public static void main(String[] args) {
+        int array [] = {1,2,4,7,7,5};
+        System.out.println(FindSecondLargest(array));
+    }
+}
