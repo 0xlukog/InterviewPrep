81b04c3dda09b88104aa1077110ff539e5b0c308
Gokul R <gk.rsjg@gmail.com>
2022-11-08 12:04:31 +0530

    move zeros to end

---
+++ Arrays/1. Easy/Move Zeros to end/MoveZerosToEnd.java
@@ -0,0 +1,24 @@
+public class MoveZerosToLeft{
+    public static int[] ZerosToLeft(int arr[]) {
+        int returnArray[] = new int[arr.length];
+        int returnArrayPosition = 0;
+        int zeroCount = 0;
+        for(int i = 0;i<arr.length;i++){
+            if(arr[i]!=0){ 
+                returnArray[returnArrayPosition] = arr[i];
+                returnArrayPosition++;
+            }
+            else{zeroCount++;}
+        }
+        for(int i = returnArrayPosition+1;i<arr.length;i++){
+            returnArray[i]=0;
+        }
+        return returnArray;
+    }
+    public static void main(String[] args) {
+        int array [] = {1 ,0 ,2 ,3 ,0 ,4 ,0 ,1};
+        for(int x : ZerosToLeft(array)){
+            System.out.print(x+" ");
+        }
+    }
+}
