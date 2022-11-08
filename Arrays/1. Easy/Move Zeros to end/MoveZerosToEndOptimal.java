9afd87b12d840ad152db838ac9e760588397f001
Gokul R <gk.rsjg@gmail.com>
2022-11-08 12:19:00 +0530

    move zeros to end optimal

--- Arrays/1. Easy/Move Zeros to end/MoveZerosToEndOptimal.java
+++ Arrays/1. Easy/Move Zeros to end/MoveZerosToEndOptimal.java
@@ -1,6 +1,22 @@
 public class MoveZerosToEndOptimal {
     private static int[] ZerosToEnd(int arr[]) {
-        return new int[1];
+        int firstZero = 0;
+        int firstNonZero = 0;
+        int arrLength = arr.length;
+        while(arr[firstZero]!=0){ // finding the first occurance of zero in the array
+            firstZero++;
+        }
+        firstNonZero = firstZero+1;
+        while(firstZero < arrLength && firstNonZero<arrLength){
+           if(arr[firstNonZero]!=0){
+            int temp = arr[firstZero];  // swapping the zero element with non zero element
+            arr[firstZero]= arr[firstNonZero];
+            arr[firstNonZero] = temp;
+            firstZero++;
+           }
+           firstNonZero++;
+    }
+        return arr;
     }
     public static void main(String[] args) {
         int array [] = {1 ,0 ,2 ,3 ,0 ,4 ,0 ,1};
