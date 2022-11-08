2bfa406365e3917c717b7e4a29456e757d2ca77e
Gokul R <gk.rsjg@gmail.com>
2022-11-08 13:21:41 +0530

    Rotate left optimal mod n

--- Arrays/1. Easy/RotateTheArrayOneLeft/RotateLeftOptimal.java
+++ Arrays/1. Easy/RotateTheArrayOneLeft/RotateLeftOptimal.java
@@ -9,14 +9,15 @@ public class RotateLeftOptimal {
         }
     }
     public static void RotateArray(int arr[], int rotateCount){
-        int n = arr.length;
-        reverseArray(arr, 0, rotateCount-1);
+        int n = arr.length; 
+        rotateCount = rotateCount%n;  // mod by n, if k is greater than n
+        reverseArray(arr, 0, rotateCount-1); 
         reverseArray(arr, rotateCount, n-1);
         reverseArray(arr, 0, n-1);
     }
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5,6};
-        RotateArray(arr,1);
+        int arr[] = {1,2,3,4,5,6,7};
+        RotateArray(arr,3);
         for(int x :arr){
             System.out.print(x+ " ");
         }
